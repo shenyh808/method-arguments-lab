2893b4cc7c9a2018c955f7a991b92090344f1479
shen huang <shenyh33@gmail.com>
2020-10-22 11:26:49 -1000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,41 @@
+lib/introduction.rb
+
+def introduction
+end
+
+def introduction_with_language
+end
+
+def introduction "josh"
+end
+
+def introduction_with_language "dan"
+    puts "hi, my name is dan and I am learning to program in Ember.js."
+end
+
+def introduction_with_language
+end
+
+def introduction "josh"
+end
+
+def introduction_with_language "dan"
+    puts "hi, my name is dan and I am learning to program in Ember.js."
+end
+
+def introduction_with_language
+end
+
+def introduction
+end
+
+def introduction "hi"
+    puts "hi"
+end
+
+def introduction "josh"
+    puts "hi, my name is josh"
+end
+
+def introduction
+end
