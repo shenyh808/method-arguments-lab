3bfd00072af66f7c8629a3c07153f2a25de45552
shen huang <shenyh33@gmail.com>
2020-10-23 10:58:14 -1000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,46 +1,9 @@
-lib/introduction.rb
-
-def introduction
+def introduction (name)
+    puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language
-end
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end 
 
-def introduction "josh"
-end
-
-def introduction_with_language "dan"
-    puts "hi, my name is dan and I am learning to program in Ember.js."
-end
-
-def introduction_with_language
-end
-
-def introduction "josh"
-end
-
-def introduction_with_language "dan"
-    puts "hi, my name is dan and I am learning to program in Ember.js."
-end
-
-def introduction_with_language
-end
-
-def introduction
-end
-
-def introduction "hi"
-    puts "hi"
-end
-
-def introduction "josh"
-    puts "hi, my name is josh"
-end
-
-def introduction
-end
-
-def introduction "josh"
-    puts "josh"
-ends
 
